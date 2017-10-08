47cc724ce6c015be833c377a70af15aa26d4db33
15600559182 <15600559182@163.com>
2017-10-08 20:42:41 +0800

    * 选择排序

--- work/BuBdemo.py
+++ work/BuBdemo.py
@@ -1,12 +1,13 @@
-#第二种方法写冒泡排序
 arr = [40, 21, 1, 5, 22, 7, 3, -1, 9]
 
+index = len(arr) - 1
 boo = True
 while boo:
     boo = False
-    for i in range(len(arr) - 1):
+    for i in range(index):
         if (arr[i] > arr[i + 1]):
             arr[i], arr[i + 1] = arr[i + 1], arr[i]
             boo = True
+    index -= 1
 
 print(arr)
